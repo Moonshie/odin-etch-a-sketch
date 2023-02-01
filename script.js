7a634ebffd70b236dc65705adc913699d1d39e58
Moonshie <moonshie.ua@gmail.com>
2023-02-01 11:42:58 +0300

    make the reset button usable, clean code

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function createStartField() {
     let container = document.querySelector(".container");
+    container.innerHTML='';
     for (let i=0; i<(16*16); i++) {
         let box = document.createElement("div")
         box.classList.add("square")
         container.appendChild(box);
     }
+    addBlackListener();
 }
 
 createStartField();
 
-const allSquares = document.querySelectorAll(".square");
-allSquares.forEach(el => el.addEventListener('mouseover', function handleClick(event)
-{event.target.classList.add('black')}));
+function addBlackListener() {
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach(el => el.addEventListener('mouseover', function handleClick(event)
+        {event.target.classList.add('black')}));
+}
