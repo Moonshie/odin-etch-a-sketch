4fcececff878d0f6e661235e7c442b8d6135d580
Moonshie <moonshie.ua@gmail.com>
2023-02-01 13:25:51 +0300

    make 10% black brush functional

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function addBlackListener() {
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach(el => el.addEventListener('mouseover', 
         function handleClick(event) {
-            event.target.style.backgroundColor = 'black';
-            event.target.style.borderColor = 'black';
+            event.target.style.backgroundColor = 'rgb(0, 0, 0)';
+            event.target.style.borderColor = 'rgb(0, 0, 0)';
             event.target.replaceWith(el.cloneNode(true));
         })
     )
@@ -37,6 +37,27 @@ function addRainbowListener() {
     )
 }
 
+function addAquaListener() {
+    removeEventListeners()
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach(el => el.addEventListener('mouseover',
+        function handleClick(event) {
+            currentColor = event.target.style.backgroundColor;
+            console.log(currentColor);
+            if (currentColor === ('rgb(0, 0, 0)')) {
+                event.target.replaceWith(el.cloneNode(true));
+            } else if (currentColor.includes('rgba')) {
+                let newColor = ampOpacity(currentColor);
+                event.target.style.backgroundColor = `rgba(0, 0, 0, ${newColor})`;
+                event.target.style.borderColor = `rgba(0, 0, 0, ${newColor})`;
+            } else {
+                event.target.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+                event.target.style.borderColor = 'rgba(0, 0, 0, 0.1)';
+            }
+        })
+    )
+}
+
 function removeEventListeners() {
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach(el => el.replaceWith(el.cloneNode(true)));
@@ -48,4 +69,15 @@ function getRandomColor() {
     const g = randomBetween(0,255);
     const b = randomBetween(0,255);
     return `rgb(${r},${g},${b})`;
+}
+
+function ampOpacity(color) {
+    let opacity = color.substr(16, 1);
+    let opacityInt = parseInt(opacity);
+    if (opacityInt === 9) {
+        return `1.0`;
+    } else {
+        opacityInt = opacityInt + 1;
+        return `0.${opacityInt}`;
+    }
 }
