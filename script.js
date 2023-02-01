8c836684087272418ff59ac1d3224cdcacf3934b
Moonshie <moonshie.ua@gmail.com>
2023-02-01 12:37:41 +0300

    make the rainbow brush functional

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ function addRainbowListener() {
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach(el => el.addEventListener('mouseover',
         function handleClick(event) {
-            event.target.style.backgroundColor = 'red';
-            event.target.style.borderColor = 'red';
+            let randomColor = getRandomColor();
+            event.target.style.backgroundColor = randomColor;
+            event.target.style.borderColor = randomColor;
         })
     )
 }
@@ -36,4 +37,12 @@ function addRainbowListener() {
 function removeEventListeners() {
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach(el => el.replaceWith(el.cloneNode(true)));
+}
+
+function getRandomColor() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min +1));
+    const r = randomBetween(0,255);
+    const g = randomBetween(0,255);
+    const b = randomBetween(0,255);
+    return `rgb(${r},${g},${b})`;
 }
